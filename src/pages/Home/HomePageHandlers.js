9589500b3eeb9ff005d1291f9e0bfc92e21359b3
CarlosEmilioAlcantara <agq.xyz@gmail.com>
2026-09-23 18:02:13 +0800

    Pusher ng commits

---
+++ src/pages/Home/HomePageHandlers.js
@@ -0,0 +1,8 @@
+import { paginateNext } from "../../utils/pagination/paginateNext";
+import { 
+  restorePositionDimension
+} from "../../utils/viewport/restorePositionDimension";
+
+const handleClickNext = (component, offset, window) => {
+
+}
